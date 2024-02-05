8740e894c99a09f8a81880a69349f971719de2ee
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-02-06 01:06:34 +0600

    solved task 1 2 and 3

---
+++ string.js
@@ -0,0 +1,46 @@
+//Task-1:
+const str1 = "I am learning string in JavaScript.";
+
+let count = 0;
+for(const letter of str1){
+    if(letter === 'a'){
+        count++;
+    }
+}
+console.log("String has the letter 'a': "+count+ " times.");
+
+console.log("==================================================================");
+
+//Task-2:
+const str2 = "A for apple.";
+
+let counter = 0;
+for(const letter of str2){
+    if(letter === 'a' || letter === 'A'){
+        counter++;
+    }
+}
+console.log("String has the letter 'a' or 'A': "+counter+ " times.");
+
+console.log("==================================================================");
+
+//Task-3:
+const str3 = "Hello";
+const str4 = "Gym";
+
+let flag = false;
+
+for(const letter of str4){
+    if(letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u'){
+        flag = true;    
+    }
+}
+if(flag === true){
+    console.log("It contains vowel.");
+}
+else{
+    console.log("It doesn't contain any vowel.");
+}
+console.log("==================================================================");
+
+//Task-4:
